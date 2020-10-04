a5a07fe0760181e7681f96445e492bf744e97b09
Rafro <Rafro@DESKTOP-GN11THH>
2020-10-03 23:53:31 -0500

    updated

--- module5_skel_student/src/main/java/com/snhu/sslserver/ServerApplication.java
+++ module5_skel_student/src/main/java/com/snhu/sslserver/ServerApplication.java
@@ -5,24 +5,57 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 
 @SpringBootApplication
 public class ServerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ServerApplication.class, args);
-	}
-
 }
 
 @RestController
 class ServerController{
+	 public static byte[] getSHA(String input) throws NoSuchAlgorithmException 
+	    {  
+	        // Static getInstance method is called with hashing SHA  
+	        MessageDigest md = MessageDigest.getInstance("SHA-256");  
+	  
+	        // digest() method called  
+	        // to calculate message digest of an input  
+	        // and return array of byte 
+	        return md.digest(input.getBytes(StandardCharsets.UTF_8));  
+	    } 
+	 
+	 public  String bytesToHex(byte[] bytes) {
+		    StringBuilder builder = new StringBuilder();
+		    for (byte b: bytes) {
+		      builder.append(String.format("%02x", b));
+		    }
+		    return builder.toString();
+		  }
+	
 //FIXME:  Add hash function to return the checksum value for the data string that should contain your name.    
     @RequestMapping("/hash")
-    public String myHash(){
-    	String data = "Hello Joe Smith!";
+    public String myHash() throws NoSuchAlgorithmException{
+    	String data = "Raphael Rose";
+    	 // Static getInstance method is called with hashing SHA  
+    	MessageDigest md = MessageDigest.getInstance("SHA-256");  
+  	  
+        // digest() method called  
+        // to calculate message digest of an input  
+        // and return array of byte 
+        byte[] hashData = md.digest(data.getBytes(StandardCharsets.UTF_8));  
+        String checkSum = bytesToHex(hashData);
+    	
        
-        return "<p>data:"+data;
+        return "<p>data:"+ data +"\n" + "Name of Cipher Algorith: SHA-256" +"\n" + "Check Sum Value: " + checkSum;
     }
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerApplication.class, args);
+		
+	}
+    
 }
